5d1209391521071a4cff24955250788ff675951f
CJ Kucera <pez@apocalyptech.com>
2020-04-10 15:26:40 -0500

    Adding a profile decoder, though I don't really have any plans to add any profile editing.  Just a convenience for myself, really.

---
+++ bl3save/bl3profile.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# vim: set expandtab tabstop=4 shiftwidth=4:
+
+# Copyright (c) 2020 CJ Kucera (cj@apocalyptech.com)
+# 
+# This software is provided 'as-is', without any express or implied warranty.
+# In no event will the authors be held liable for any damages arising from
+# the use of this software.
+# 
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software in a
+#    product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 
+# 3. This notice may not be removed or altered from any source distribution.
+
+# The encryption/decryption stanzas in BL3Profile.__init__
+# were helpfully provided by Gibbed (rick 'at' gibbed 'dot' us), so many
+# thanks for that!  https://gist.github.com/gibbed/b6a93f74c575ce99b42c3b629ac1856a
+#
+# The rest of the savegame format was gleaned from 13xforever/Ilya's
+# "gvas-converter" project: https://github.com/13xforever/gvas-converter
+
+import base64
+import struct
+import google.protobuf
+from . import *
+from . import OakProfile_pb2, OakShared_pb2
+
+class BL3Profile(object):
+    """
+    """
+
+    _prefix_magic = bytearray([
+        0xD8, 0x04, 0xB9, 0x08, 0x5C, 0x4E, 0x2B, 0xC0,
+        0x61, 0x9F, 0x7C, 0x8D, 0x5D, 0x34, 0x00, 0x56,
+        0xE7, 0x7B, 0x4E, 0xC0, 0xA4, 0xD6, 0xA7, 0x01,
+        0x14, 0x15, 0xA9, 0x93, 0x1F, 0x27, 0x2C, 0x8F,
+        ])
+
+    _xor_magic = bytearray([
+        0xE8, 0xDC, 0x3A, 0x66, 0xF7, 0xEF, 0x85, 0xE0,
+        0xBD, 0x4A, 0xA9, 0x73, 0x57, 0x99, 0x30, 0x8C,
+        0x94, 0x63, 0x59, 0xA8, 0xC9, 0xAE, 0xD9, 0x58,
+        0x7D, 0x51, 0xB0, 0x1E, 0xBE, 0xD0, 0x77, 0x43,
+        ])
+
+    def __init__(self, filename, debug=False):
+        self.filename = filename
+        with open(filename, 'rb') as df:
+
+            header = df.read(4)
+            assert(header == b'GVAS')
+
+            self.sg_version = self._read_int(df)
+            if debug:
+                print('Savegame version: {}'.format(self.sg_version))
+            self.pkg_version = self._read_int(df)
+            if debug:
+                print('Package version: {}'.format(self.pkg_version))
+            self.engine_major = self._read_short(df)
+            self.engine_minor = self._read_short(df)
+            self.engine_patch = self._read_short(df)
+            self.engine_build = self._read_int(df)
+            if debug:
+                print('Engine version: {}.{}.{}.{}'.format(
+                    self.engine_major,
+                    self.engine_minor,
+                    self.engine_patch,
+                    self.engine_build,
+                    ))
+            self.build_id = self._read_str(df)
+            if debug:
+                print('Build ID: {}'.format(self.build_id))
+            self.fmt_version = self._read_int(df)
+            if debug:
+                print('Custom Format Version: {}'.format(self.fmt_version))
+            fmt_count = self._read_int(df)
+            if debug:
+                print('Custom Format Data Count: {}'.format(fmt_count))
+            self.custom_format_data = []
+            for _ in range(fmt_count):
+                guid = self._read_guid(df)
+                entry = self._read_int(df)
+                if debug:
+                    print(' - GUID {}: {}'.format(guid, entry))
+                self.custom_format_data.append((guid, entry))
+            self.sg_type = self._read_str(df)
+            if debug:
+                print('Savegame type: {}'.format(sg_type))
+
+            # Read in the actual data
+            remaining_data_len = self._read_int(df)
+            data = bytearray(df.read(remaining_data_len))
+
+            # Decrypt
+            for i in range(len(data)-1, -1, -1):
+                if i < 32:
+                    b = BL3Profile._prefix_magic[i]
+                else:
+                    b = data[i - 32]
+                b ^= BL3Profile._xor_magic[i % 32]
+                data[i] ^= b
+
+            # Make sure that was all there was
+            last = df.read()
+            assert(len(last) == 0)
+
+            # Parse protobufs
+            self.import_protobuf(data)
+
+    def import_protobuf(self, data):
+        """
+        Given raw protobuf data, load it into ourselves so
+        that we can work with it.  This also sets up a few
+        convenience vars for our later use
+        """
+
+        # Now parse the protobufs
+        self.prof = OakProfile_pb2.Profile()
+        self.prof.ParseFromString(data)
+
+    def _read_int(self, df):
+        return struct.unpack('<I', df.read(4))[0]
+
+    def _write_int(self, df, value):
+        df.write(struct.pack('<I', value))
+
+    def _read_short(self, df):
+        return struct.unpack('<H', df.read(2))[0]
+
+    def _write_short(self, df, value):
+        df.write(struct.pack('<H', value))
+
+    def _read_str(self, df):
+        datalen = self._read_int(df)
+        if datalen == 0:
+            return None
+        elif datalen == 1:
+            return ''
+        else:
+            value = df.read(datalen)
+            return value[:-1].decode('utf-8')
+
+    def _write_str(self, df, value):
+        if value is None:
+            self._write_int(df, 0)
+        elif value == '':
+            self._write_int(df, 1)
+        else:
+            data = value.encode('utf-8') + b'\0'
+            self._write_int(df, len(data))
+            df.write(data)
+
+    def _read_guid(self, df):
+        data = df.read(16)
+        return data
+        # A bit silly to bother formatting it, since we don't care.
+        #arr = ''.join(['{:02x}'.format(d) for d in data])
+        #return '{}-{}-{}-{}-{}'.format(
+        #        arr[0:8],
+        #        arr[8:12],
+        #        arr[12:16],
+        #        arr[16:20],
+        #        arr[20:32],
+        #        )
+
+    def _write_guid(self, df, value):
+        df.write(value)
+
