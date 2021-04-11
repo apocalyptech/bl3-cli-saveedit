dfab3c65e173a36a90e93c6c93c8d193805ea825
CJ Kucera <pez@apocalyptech.com>
2021-04-11 15:13:56 -0500

    Updating balance mapping filename in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         package_data={
             'bl3save': [
                 'resources/inventoryserialdb.json.xz',
-                'resources/short_name_balance_mapping.json.xz',
+                'resources/balance_name_mapping.json.xz',
                 'resources/balance_to_inv_key.json.xz',
                 ],
             },
